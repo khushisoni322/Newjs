2316a8e56b15abd2230ecb0f233bf53e7b2153a2
khushisoni322 <sonikhushi81032@gmail.com>
2026-07-05 09:00:59 +0530

    learn string

--- Jsseries/chai1.js
+++ Jsseries/chai1.js
@@ -1,13 +1,64 @@
-const accountId = 1229939;
-let accountEmail="khushi23@gmail.com";
-var accountPassword = "jhfjs23";
- var accountCity = "delhi";
+// const accountId = 1229939;
+// let accountEmail="khushi23@gmail.com";
+// var accountPassword = "jhfjs23";
+//  var accountCity = "delhi";
 
-console.table([accountId,accountEmail,accountPassword,accountCity]);
-
-
-// prefer to not use var beacause it is function scoped 
-// and can lead to unexpected behavior. Use let or const instead for block scoping.
+// console.table([accountId,accountEmail,accountPassword,accountCity]);
 
 
-"use strict"; // treat js as newer version 
+// // prefer to not use var beacause it is function scoped 
+// // and can lead to unexpected behavior. Use let or const instead for block scoping.
+
+
+// "use strict"; // treat js as newer version 
+
+// // data types:-
+
+// let age = 18;
+// let isStudent = true;
+// let name = "John don";
+// console.log(typeof age); // number
+// console.log(typeof isStudent); // boolean
+// console.log(typeof name); // string
+
+
+// // ********** premitive data types  **********//
+
+// // 7 types- string , number , boolean, null, undefined, symbol, bigint
+
+// // ************ non premitive data type ( reference type ) **********//
+
+// // arrays , obj , and functions are non premitive data types
+
+// // lets see conversion of data types;
+
+// let isLoggedIn = 1;
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn); // convert number to boolean
+// console.log(booleanIsLoggedIn);
+
+// // if we only do  it gives = " " -> false, and "khushui" -> true, and 0 -> false
+
+// //************************ operations  ************************//
+
+// let str1 = "hello"
+// let str2="  khushi ji"
+// let str3= str1 + str2;
+// console.log(str3);
+
+// // conversion cases :- based on ecmas rule  and plzz avoid these conversion 
+
+// console.log(null>0); // false
+// console.log(null<0); // false
+// console.log(null>=0); // true
+// console.log(null==0); // false
+// console.log(null===0); // false its stricts checks 
+
+// console.log(undefined>0); // false;
+// console.log(undefined<0); // false;
+// console.log(undefined>=0); // false;
+// console.log(undefined==0); // false;
+// console.log(undefined===0); // false;
+
+
+// // uhuiuywopshjdhshi

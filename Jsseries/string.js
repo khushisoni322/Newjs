01f92a847638b40ae18ec2eb1445ab618e1afe36
khushisoni322 <sonikhushi81032@gmail.com>
2026-07-05 10:31:03 +0530

    maths and num

--- Jsseries/string.js
+++ Jsseries/string.js
@@ -1,29 +1,29 @@
-console.log("string");
+// console.log("string");
 
-// * string declaration :-
-const name = "khushi"; // method 1
-const gameName = new String ('temolerun'); // method 2
-console.log(gameName); // here string will be obj in key value pairs 
+// // * string declaration :-
+// const name = "khushi"; // method 1
+// const gameName = new String ('temolerun'); // method 2
+// console.log(gameName); // here string will be obj in key value pairs 
 
-// * string concatenation :-
-const repoCount = 87;
-// console.log(name +  repoCount );  do not use it  syntax;
+// // * string concatenation :-
+// const repoCount = 87;
+// // console.log(name +  repoCount );  do not use it  syntax;
 
-console.log(`my name is ${name}  and my repoCount is ${repoCount}`); // good way of syntax
+// console.log(`my name is ${name}  and my repoCount is ${repoCount}`); // good way of syntax
 
 
-console.log(gameName.charAt(2)); // it will give the character at index 2
-console.log(gameName.indexOf('u')); // it will give the index of character u
+// console.log(gameName.charAt(2)); // it will give the character at index 2
+// console.log(gameName.indexOf('u')); // it will give the index of character u
 
-const newString = gameName.substring(0,3); // it will give the string from index 0 to 3
-console.log(newString);
+// const newString = gameName.substring(0,3); // it will give the string from index 0 to 3
+// console.log(newString);
 
-const newString2= gameName.slice(-7,4); // it will give the string from index -7 to 4
-console.log(newString2);
+// const newString2= gameName.slice(-7,4); // it will give the string from index -7 to 4
+// console.log(newString2);
 
-const url = "https://www.khushi%93soni.com";
-console.log(url.replace("%93","%96")); // it will replace the %93 with %96
+// const url = "https://www.khushi%93soni.com";
+// console.log(url.replace("%93","%96")); // it will replace the %93 with %96
 
-console.log(url.includes("khushi")); // it will check if the string includes "khushi" or not
-console.log(url.includes("aajk"));
+// console.log(url.includes("khushi")); // it will check if the string includes "khushi" or not
+// console.log(url.includes("aajk"));
 
